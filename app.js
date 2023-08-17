a746cb85237b14dcec7b13c71eee48b09e1877e3
AyBi <113614075+AyBi229@users.noreply.github.com>
2023-08-17 03:03:18 +0100

    setting colors for candies (blue and yellow)

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
-var canvas = document.getElementById("yellow");
-var context = canvas.getContext("2d");
+var canvases = document.getElementsByClassName("candy");
+Array.from(canvases).forEach(canvas => {
+    var context = canvas.getContext("2d");
 
-var centerX = canvas.width / 2;
-var centerY = canvas.height / 2;
-var radius = 25;
-var fillColor = "yellow";
+    var centerX = canvas.width / 2;
+    var centerY = canvas.height / 2;
+    var radius = 25;
+    var fillColor = null
+    switch(canvas.id){
+        case 'yellow':
+            fillColor = 'yellow';
+            break;
+        case 'blue':
+            fillColor = 'blue';
+            break;
+    }
 
-context.beginPath();
-context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
-context.fillStyle = fillColor;
-context.fill();
-context.closePath();
-console.log((100*50)/400);
+    context.beginPath();
+    context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+    context.fillStyle = fillColor;
+    context.fill();
+    context.closePath();
+});
+
+//console.log((100*50)/400);
